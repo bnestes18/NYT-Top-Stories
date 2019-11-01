7e2c4a2537a9297d36e0dc7a63e9c123e643a849
Brittney Estes <bnestes2014@gmail.com>
2019-10-31 21:31:53 -0400

    Add initial functionality for NYT Top Stories news feed

---
+++ topStories.js
@@ -0,0 +1,26 @@
+;(function () {
+
+    let url = 'https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=OCuAzfRfKVr1HDAkN1lCOImvrRs8Rlwm'
+    let app = document.querySelector('#app');
+
+    fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            } 
+        }).then(function (data) {
+            app.innerHTML = '<div>' + data.results.map(function (result) {
+                let { abstract, byline, title } = result;
+                    return '<h2><a href="#>' + title + '</a></h2>' + '<p>' + byline + '</p>' + '<p>' + abstract + '</p>'
+            }).join('') + '</div>';
+        }).catch(function (err) {
+            console.log(err);
+            app.innerHTML = '<p>Unable to show stories</p>'
+        });
+
+})();
+
+
+
