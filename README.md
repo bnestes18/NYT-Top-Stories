2c191add6fc477f5f0c4d7cb3415e1f796758d11
Brittney Estes <bnestes2014@gmail.com>
2019-11-04 21:55:08 -0500

    Add functionality to display stories from multiple categories.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NYT-Top-Stories
-This project uses the New York Times API to display their top sports stories of the day on a webpage.
+Part 1
+This part of the project uses the New York Times API to display their top sports stories of the day on a webpage.
+
+Part 2
+This part of the project uses the New York Times API to display stories of the day from multiple categories (automobiles, sports and technology)  on a webpage.
--- topStories.js
+++ topStories.js
@@ -1,25 +1,56 @@
 ;(function () {
 
-    let url = 'https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=OCuAzfRfKVr1HDAkN1lCOImvrRs8Rlwm'
+    let apiKey = 'OCuAzfRfKVr1HDAkN1lCOImvrRs8Rlwm';
     let app = document.querySelector('#app');
+    let sections = ['automobiles', 'sports', 'technology'];
 
-    fetch(url)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            } 
-        }).then(function (data) {
-            app.innerHTML = '<div>' + data.results.map(function (result) {
-                let { abstract, byline, title, url } = result;
-                    return '<h2><a href="'+ url +'">' + title + '</a></h2>' + '<p>' + byline + '</p>' + '<p>' + abstract + '</p>'
-            }).join('') + '</div>';
-        }).catch(function (err) {
-            console.log(err);
-            app.innerHTML = '<p>Unable to show stories</p>'
-        });
+    
+        // Get the articles from a particular section
+        let getArticles = function (section) {
+            fetch('https://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=' + apiKey).then(function (response) {
+                // Check the response after fetch
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return Promise.reject(response);
+                }
+            }).then(function(data) {
 
+                // Get the first few articles
+                let firstFew = getFewArticles(data.results);
+
+                // Render the first few articles to the DOM
+                render(firstFew, section);
+            })
+            .catch(function(err) {
+                console.log('Cannot display stories', err);
+            });
+        }
+            
+        // Get all articles for each section
+        sections.forEach(function (section) {
+            getArticles(section);
+        })
+
+        let getFewArticles = function (articles) {
+            return articles.slice(0, 4);
+        };
+
+        let render = function (articles, section) {
+            // Create a new array of markup strings with array.map(), then
+            // combine them into one string with array.join(), then
+            // insert them into the DOM with innerHTML
+
+            app.innerHTML += '<h2>' + section + '</h2>' + articles.map(function (article) {
+                let html = 
+                    '<article>' +
+                        '<h3><a href="' + article.url + '">' + article.title + '</a></h3>' +
+                        '<p>' + article.byline + '</p>' +
+                        '<p>' + article.abstract + '</p>' +
+                    '</article>';
+                    return html;
+            }).join('');
+        }
 })();
 
 
